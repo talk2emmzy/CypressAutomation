cd123859de360285a015ff1f2307dfea4a1e4137
Emmanuel Okoye <76595231+talk2emmzy@users.noreply.github.com>
2022-10-08 19:49:32 +0100

    Update FirstTest.js

--- cypress/integration/1-getting-started/FirstTest.js
+++ cypress/integration/1-getting-started/FirstTest.js
@@ -9,22 +9,22 @@ describe('TEST SUITE', () => {
      Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
       });
-    // cy.get("#inputPassword").type("123456789")
-    // cy.get("[ng-hide='loginVm.logging']").click()
+    cy.get("#inputPassword").type("123456789")
+    cy.get("[ng-hide='loginVm.logging']").click()
     
    
 })
-    //it('verify page title Positive', () => 
-    //{
-    //cy.visit('https://oms.omnibiz.com/#/')
-   //cy.title().should('eq','Login')
-    //cy.get("input.form__input").type("okoye@omnibiz.com")
-    //})
+    it('verify page title Positive', () => 
+    {
+    cy.visit('https://oms.omnibiz.com/#/')
+    cy.title().should('eq','Login')
+    cy.get("input.form__input").type("okoye@omnibiz.com")
+    })
 
-    //it('verify page title Positive', () => 
-    //{
-    //cy.visit('https://oms.omnibiz.com/#/')
-    //cy.title().should('eq','Login')
-    //cy.get("input.form__input").type("okoye@omnibiz.com")
-   //})
+    it('verify page title Positive', () => 
+    {
+    cy.visit('https://oms.omnibiz.com/#/')
+    cy.title().should('eq','Login')
+    cy.get("input.form__input").type("okoye@omnibiz.com")
+   })
   })
